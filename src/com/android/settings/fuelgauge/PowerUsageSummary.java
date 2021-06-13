24ca87b32e0f257db400f417b78d60101cfc1881
pritish <pritish@stag-os.org>
2021-06-13 16:09:00 +0200

    Revert "Revert "Update related UI if battery is not present""
    This reverts commit 101c5e94db79edf0b915356e94637ea68536825e.

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -66,8 +66,8 @@ import com.android.settingslib.widget.LayoutPreference;
 import java.util.List;
 
 /**
- * Displays a list of apps and subsystems that consume power, ordered by how much power was
- * consumed since the last time it was unplugged.
+ * Displays a list of apps and subsystems that consume power, ordered by how much power was consumed
+ * since the last time it was unplugged.
  */
 @SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
 public class PowerUsageSummary extends PowerUsageBase implements OnLongClickListener,
@@ -243,7 +243,9 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
                 KEY_TIME_SINCE_LAST_FULL_CHARGE);
         mBatteryUtils = BatteryUtils.getInstance(getContext());
 
-        restartBatteryInfoLoader();
+        if (Utils.isBatteryPresent(getContext())) {
+            restartBatteryInfoLoader();
+        }
         mBatteryTipPreferenceController.restoreInstanceState(icicle);
         updateBatteryTipFlag(icicle);
     }
@@ -320,6 +322,10 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         if (context == null) {
             return;
         }
+        // Skip refreshing UI if battery is not present.
+        if (!mIsBatteryPresent) {
+            return;
+        }
 
         // Skip BatteryTipLoader if device is rotated or only battery level change
         if (mNeedUpdateBatteryTip
@@ -328,7 +334,6 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         } else {
             mNeedUpdateBatteryTip = true;
         }
-
         // reload BatteryInfo and updateUI
         restartBatteryInfoLoader();
         updateLastFullChargePreference();
@@ -449,6 +454,10 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
     @VisibleForTesting
     void restartBatteryInfoLoader() {
         if (getContext() == null) {
+            return;
+        }
+        // Skip restartBatteryInfoLoader if battery is not present.
+        if (!mIsBatteryPresent) {
             return;
         }
         getLoaderManager().restartLoader(BATTERY_INFO_LOADER, Bundle.EMPTY,
